b53a7bd1068ff06e73bbe6418a94f989caae9e6d
hanwankui <wankui.han@tengwow.com>
2022-12-16 16:25:02 +0800

    初始化

--- lib/src/App.dart
+++ lib/src/App.dart
@@ -4,49 +4,52 @@ import './pages/my_page.dart';
 import './pages/search_page.dart';
 import './pages/travel_page.dart';
 class App extends StatefulWidget {
-   App({ Key? key,required this.title }) : super(key: key);
-   String title;
+   const App({ Key? key}) : super(key: key);
   @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  final List _currentPages =[
-    const HomePage(),
-    const TravelPage(),
-    const SearchPage(),
-    const MyPage(),
+  static const List _currentPages =[
+    HomePage(),
+    TravelPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+  static const List<BottomNavigationBarItem> _BottomTabBars = [
+     BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: "首页"
+    ),
+     BottomNavigationBarItem(
+      icon: Icon(Icons.flight_class),
+      label: "分类"
+    ),
+       BottomNavigationBarItem(
+      icon: Icon(Icons.shopping_cart),
+      label: "购物车"
+    ),
+       BottomNavigationBarItem(
+      icon: Icon(Icons.account_circle),
+      label: "我的"
+    ),
   ];
   int _currentIndex=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("测试页面"),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex:_currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: _BottomTabBars,
         onTap: (value) {
           setState(() {
             _currentIndex = value;
           });
-        },
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "首页"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera_alt),
-            label: "旅拍"
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "搜索"
-          ),
-           BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "我的"
-          ),
-        ],
+        }
       ),
       body: _currentPages[_currentIndex],
     );
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:card_swiper/card_swiper.dart';
+// import 'package:card_swiper/card_swiper.dart';
+import 'package:dio/dio.dart';
 const APPBAR_SCROLL_OFFSET = 100;
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,90 +10,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List _imageUrls = [
-    'https://www.itying.com/images/flutter/1.png',
-    'https://www.itying.com/images/flutter/2.png',
-    'https://www.itying.com/images/flutter/3.png',
-    // 'https://www.itying.com/images/flutter/4.png',
-    // 'https://www.itying.com/images/flutter/5.png',
-    // 'https://www.itying.com/images/flutter/6.png',
-    // 'https://www.itying.com/images/flutter/7.png',
-  ];
-  double appBarAlpha = 0;
-  _onScroll(Offset){
-    double alpha = Offset/APPBAR_SCROLL_OFFSET;
-    if(alpha<0){
-      alpha = 0;
-    }else if(alpha>1){
-      alpha = 1;
+  void _getHttp() async {
+    print("触发了");
+    try {
+      var response = await Dio().get('http://www.baidu.com');
+      print(response);
+    } catch (e) {
+      print(e);
+    } finally{
+      print("66666");
     }
-    setState(() {
-      appBarAlpha = alpha;
-    });
-    print(Offset);
   }
   @override
   Widget build(BuildContext context) {
+     _getHttp();
     return Scaffold(
-        body: Stack(
-          children: [
-            MediaQuery.removePadding(
-              context: context, 
-              removeTop: true,
-              child: NotificationListener(
-                onNotification:(notification) {
-                  if (notification is ScrollUpdateNotification && notification.depth==0) {
-                    _onScroll(notification.metrics.pixels);
-                  }
-                  return false;
-                },
-                child: ListView(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 160,
-                            child: Swiper(
-                              itemCount: _imageUrls.length,
-                              autoplay: true,
-                              loop: true,
-                              pagination: const SwiperPagination(),
-                              itemBuilder: (context, index) {
-                                return Image.network(
-                                  _imageUrls[index],
-                                  fit: BoxFit.cover,
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 800,
-                      child: Text("23456"),
-                    )
-                  ],
-                ),
-              )
+      
+      body: Column(
+        mainAxisAlignment:MainAxisAlignment.start,
+        children:  [
+          TextField(
+            decoration: const InputDecoration(
+              contentPadding: EdgeInsets.all(10),
+              labelText: '美女类型',
+              helperText: '请输入你喜欢的类型'
             ),
-            Opacity(
-              opacity: appBarAlpha,
-              child: Container(
-                height: 80,
-                decoration: const BoxDecoration(
-                  color: Colors.white
-                ),
-                child:const Expanded(
-                  child:Center(
-                    child: Text("首页"),
-                  )
-                )
-              ),
-            )
-          ],
-        )
-      );
+            onChanged:(value) {
+              print(value);
+            },
+            onSubmitted: (value) {
+              print(value);
+            },
+          )
+        ]
+      )
+    );
   }
 }
